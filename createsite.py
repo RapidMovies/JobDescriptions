4a913043e4a1f84c79c491654e12feaa6b2cd94b
rafeedarian99 <rafeedarian99@gmail.com>
2025-01-01 19:48:55 -0700

    -

--- createsite.py
+++ createsite.py
@@ -1,14 +1,11 @@
 import sys
 import os
-from tld import get_tld
 
 site = sys.argv[1]
 assert '/' in site, f'"/" does not appear in "{site}"'
-
-domain_name = get_tld(site, as_object=True).fld
-site = site[site.index(domain_name):]
-site.strip("/")
+site = site[site.index('//')+2:]
 site = "site/" + site
+print(site)
 
 os.makedirs(site)
 os.system(f"cp target.html {site}/index.html")
