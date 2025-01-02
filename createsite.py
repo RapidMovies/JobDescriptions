1da691b4ff828def351d1411d12de51e9852ab6d
rafeedarian99 <rafeedarian99@gmail.com>
2025-01-01 19:29:09 -0700

    -

--- createsite.py
+++ createsite.py
@@ -7,5 +7,8 @@ assert '/' in site, f'"/" does not appear in "{site}"'
 
 domain_name = get_tld(site, as_object=True).fld
 site = site[site.index(domain_name):]
+site.strip("/")
+site = "site/" + site
 
 os.makedirs(site)
+os.system(f"cp target.html {site}/index.html")
