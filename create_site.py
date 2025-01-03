f342bf0ab97262bb2828f1fcfa836d927d5229b0
rafeedarian99 <rafeedarian99@gmail.com>
2025-01-02 18:00:00 -0700

    -

---
+++ create_site.py
@@ -0,0 +1,43 @@
+import os
+
+def create_site(site, title, target):
+    ## ADD NEW SITE ##]
+    assert '/' in site, f'"/" does not appear in "{site}"'
+    site = site[site.index('//')+2:]
+    site = "site/" + site
+    print('https://rapidmovies.github.io/JobDescriptions/' + site)
+
+    try:
+        os.makedirs(site)
+    except FileExistsError:
+        pass
+    target = f"<!-- {title} -->\n" + target
+    with open(f'{site}/index.html', 'w') as f:
+        f.write(target)
+
+    ## UPDATE INDEX.HTML ##
+
+    # TODO: Fix Index.HTML Update
+    # all_pages = []
+    # for root, _, files in os.walk('site'):
+    #     for file in files:
+    #         assert file == 'index.html', f'What is {file} doing at {root}?'
+    #         with open(f"{root}/{file}") as f:
+    #             title = f.read().split('\n')[0][5:-4]
+    #         all_pages.append(f'    <li><a href="./{root}">{title}</li>')
+    # all_pages = "\n".join(all_pages)
+
+    # html = f"""<html xmlns="http://www.w3.org/1999/xhtml" >
+    # <head>
+    #     <title>Job Descriptions</title>
+    # </head>
+    # <body>
+    # <h1>Job Descriptions</h1>
+    # <ol>
+    # {all_pages}
+    # </o1>
+    # </body>
+    # </html>"""
+
+    # with open('index.html', 'w') as f:
+    #     f.write(html)
--- extract_info.py
+++ extract_info.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
 import re
+from create_site import create_site
+from datetime import datetime
+import sys
+
+assert len(sys.argv) == 3, f"Correct Usage: python {sys.argv[0]} <URL> <REQID>"
+site = sys.argv[1]
+reqid = sys.argv[2]
 
 with open("target.html") as f:
     soup = BeautifulSoup(f.read(), "html.parser")
 
 desc = soup.find("div", {"id": "job-details"})
 position = soup.find("h1", {"class": "t-24"}).text
+position = re.sub(r"\s+", r" ", position)
 
 _details = soup.find(
     "div", {"class": "job-details-jobs-unified-top-card__primary-description-container"}
@@ -17,9 +25,26 @@ company = _company_box.find("div", {"class": "artdeco-entity-lockup__title"})
 company = company.text.strip()
 _details = _company_box.find_all("div", {"class": "t-14"})
 _details = next(d for d in _details if "mt5" in d["class"]).text.strip()
-_match = re.match(r'([^\n]*)\s*([^\s].*) employees\s*([^\s].*) on LinkedIn', _details)
+_match = re.match(r"([^\n]*)\s*([^\s].*) employees\s*([^\s].*) on LinkedIn", _details)
 industry = _match.group(1)
 employees = _match.group(2)
 li_employees = _match.group(3)
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-print([company, industry, employees, li_employees, position, '#REQID', location])
+row_to_add = [
+    company,
+    industry,
+    employees,
+    li_employees,
+    position,
+    reqid,
+    location,
+    now,
+    "",
+    "pending",
+]
+row_to_add = ",".join(f'"{e}"' for e in row_to_add)
+with open("job_application_tracker.csv", "a") as f:
+    f.write(row_to_add + "\n")
+
+create_site(site, position, str(desc))
